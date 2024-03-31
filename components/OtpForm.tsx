dd048b04680602745f82c12becc8dcc2e8de9e37
Ehsan Khan <shofirayhan07112000@gmail.com>
2024-03-31 23:04:18 +0600

    updated otp form

--- components/OtpForm.tsx
+++ components/OtpForm.tsx
@@ -1,14 +1,35 @@
 "use client";
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Alert } from "@/components/ui/alert";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next-nprogress-bar";
 
-const OtpForm = () => {
+import { useToast } from "./ui/use-toast";
+import { Loader2 } from "lucide-react";
+
+type Session = {
+  NID: string;
+  phone: string;
+} | null;
+
+const OtpForm = ({ session }: { session: Session }) => {
   const router = useRouter();
   const [input, setInput] = useState("");
-  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { toast } = useToast();
+
   const handleOTP = async () => {
+    setLoading(true);
     try {
       const res = await fetch("/api/validateOTP", {
         method: "POST",
@@ -18,30 +39,61 @@ const OtpForm = () => {
         body: JSON.stringify({ otp: input }),
       });
 
-      if (res.status == 200) {
-        router.push("/vote");
-      }
-
       const data = await res.json();
 
-      setMessage(data.message);
+      if (res.status == 200) {
+        router.push("/vote");
+      } else {
+        toast({
+          title: data.message,
+          variant: "destructive",
+        });
+        if (data.message === "OTP has expired!") {
+          router.push("//vote/verification");
+        }
+        setLoading(false);
+      }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
   return (
-    <div>
-      <form action={handleOTP}>
-        <Input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <Button type="submit">Verify</Button>
-      </form>
-
-      <p>{message}</p>
-    </div>
+    <Card className="w-full max-w-2xl mx-auto">
+      <CardHeader className="flex flex-col items-center space-y-1">
+        <CardTitle>OTP Verification</CardTitle>
+        <CardDescription>
+          Enter the OTP sent to {session?.phone} to verify your identity.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <div className="flex space-x-4">
+          <Input
+            className="font-mono"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="••••••"
+            required
+          />
+        </div>
+        {loading ? (
+          <Button className="w-full" disabled>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Please wait
+          </Button>
+        ) : (
+          <Button className="w-full" onClick={handleOTP}>
+            Verify OTP
+          </Button>
+        )}
+      </CardContent>
+      <CardFooter>
+        <Alert className="text-sm">
+          OTP is valid for 2 minutes. Please check your phone for the OTP.
+        </Alert>
+      </CardFooter>
+    </Card>
   );
 };
 
