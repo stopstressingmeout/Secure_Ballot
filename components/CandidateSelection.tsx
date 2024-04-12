3a549e5e7c1b03449011ed7fbbf1c88b2a309c7b
Ehsan Khan <shofirayhan07112000@gmail.com>
2024-04-12 19:12:15 +0600

    added model in candidate selection

--- components/CandidateSelection.tsx
+++ components/CandidateSelection.tsx
@@ -9,6 +9,19 @@ import {
 } from "@/components/ui/card";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+import { useToast } from "./ui/use-toast";
 
 type Voter = {
   name: string;
@@ -36,11 +49,31 @@ type CandidateSelectionProps = {
   constituency: Constituency;
 };
 
+type ConfirmVote = {
+  candidateId: number;
+  constituencyName: string;
+  candidateName: string;
+  candidateImage: string;
+  candidateParty: string;
+};
+
 const CandidateSelection = ({
   voter,
   constituency,
 }: CandidateSelectionProps) => {
-  const handleVote = async (candidateId: number) => {
+  const { toast } = useToast();
+
+  const [selectedCandidate, setSelectedCandidate] =
+    useState<ConfirmVote | null>(null);
+
+  const handleVote = async (candidateId: ConfirmVote | null) => {
+    if (candidateId === null) {
+      toast({
+        title: "Something went wrong. Please try again!",
+        variant: "destructive",
+      });
+      return;
+    }
     const response = await fetch("/api/confirmVote", {
       method: "POST",
       headers: {
@@ -60,38 +93,81 @@ const CandidateSelection = ({
     }
   };
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 w-full max-w-5xl mx-auto">
-      {constituency.parties.map((party, index) => (
-        <Card key={index}>
-          <CardHeader>
-            <div className="flex justify-center items-center gap-5">
-              <Avatar className="rounded-full">
-                <AvatarImage src={party.logoUrl} alt={party.partyName} />
-              </Avatar>
-              <Avatar className="rounded-full">
-                <AvatarImage
-                  src={party.candidateImage}
-                  alt={party.candidateName}
-                />
-              </Avatar>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <CardTitle>{party.candidateName}</CardTitle>
-            <CardDescription>{party.partyName}</CardDescription>
-          </CardContent>
-          <CardFooter className="flex justify-center items-center">
-            <Button
-              onClick={() => handleVote(party.candidateId)}
-              size={"lg"}
-              className="w-1/3"
-            >
-              Vote
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <AlertDialog>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 w-full max-w-5xl mx-auto">
+        <AlertDialogContent className="flex flex-col justify-center items-center gap-4 text-center">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-center">
+              Confirm Your Vote
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              <CardHeader>
+                <div className="flex justify-center items-center gap-5">
+                  <Avatar className="rounded-full">
+                    <AvatarImage
+                      src={selectedCandidate?.candidateImage}
+                      alt={selectedCandidate?.candidateImage}
+                    />
+                  </Avatar>
+                </div>
+              </CardHeader>
+              <CardContent className="text-center">
+                <CardTitle>{selectedCandidate?.candidateName}</CardTitle>
+                <CardDescription>
+                  {selectedCandidate?.candidateParty}
+                </CardDescription>
+              </CardContent>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => handleVote(selectedCandidate)}>
+              Confirm Vote
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+
+        {constituency.parties.map((party, index) => (
+          <Card key={index}>
+            <CardHeader>
+              <div className="flex justify-center items-center gap-5">
+                <Avatar className="rounded-full">
+                  <AvatarImage src={party.logoUrl} alt={party.partyName} />
+                </Avatar>
+                <Avatar className="rounded-full">
+                  <AvatarImage
+                    src={party.candidateImage}
+                    alt={party.candidateName}
+                  />
+                </Avatar>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <CardTitle>{party.candidateName}</CardTitle>
+              <CardDescription>{party.partyName}</CardDescription>
+            </CardContent>
+            <CardFooter className="flex justify-center items-center">
+              <AlertDialogTrigger asChild>
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    setSelectedCandidate({
+                      candidateId: party.candidateId,
+                      constituencyName: constituency.constituencyName,
+                      candidateName: party.candidateName,
+                      candidateImage: party.candidateImage,
+                      candidateParty: party.partyName,
+                    });
+                  }}
+                >
+                  Vote
+                </Button>
+              </AlertDialogTrigger>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </AlertDialog>
   );
 };
 
