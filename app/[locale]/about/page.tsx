de28b5aebbf237665427623eeeceb29ce60dbdb9
Fatema Tuz Zohra <fatema.promy456@gmail.com>
2024-05-05 00:53:01 +0600

    about section translation done

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -15,18 +15,7 @@ const AboutPage = ({ params: { locale } }: PageProps) => {
         <div>
           <h1 className="text-3xl mb-5">{t("title")}</h1>
           <h1 className="font-light">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nam
-            repellat facilis veniam laudantium et alias exercitationem quas,
-            atque sequi omnis rem iure perspiciatis adipisci qui esse dolorum
-            harum hic! Illo fugit ab voluptate ducimus nemo natus expedita,
-            voluptatum, ad delectus quisquam similique? Nemo repellat laudantium
-            natus, id dolorum dolorem suscipit voluptate corrupti nobis quaerat
-            aliquam dicta laboriosam cumque aut quae nulla pariatur! Voluptatem
-            impedit voluptas voluptate vitae, laudantium quidem ullam aut ea
-            atque saepe quisquam ipsam? Adipisci facilis dolore cupiditate
-            accusantium unde? Fugit sint consequatur repellat soluta quo maxime
-            quaerat quisquam amet magnam, quibusdam sunt quidem deleniti aperiam
-            ducimus voluptate dolorem in distinctio minus repudiandae numquam.
+            {t("description")}
           </h1>
         </div>
         <div className="md:flex justify-center items-center hidden">
